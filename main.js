7c0105fa2560ff8cdce01849e957ae4e9f94a4e8
Asyrique <asyrique@vase.ai>
2023-03-04 13:49:25 +0800

    added days and seconds

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const asiatime = document.querySelector("#asiatime")
 
 setInterval(() => {
   const date = new Date()
-  uswesttime.innerText = (date.toLocaleString("en-US", {timeZone: "America/Anchorage"})).replace(", ", " - ")
-  useasttime.innerText = (date.toLocaleString("en-US", {timeZone: "America/Chicago"})).replace(", ", " - ")
-  europetime.innerText = (date.toLocaleString("en-US", {timeZone: "Atlantic/Cape_Verde"})).replace(", ", " - ")
-  asiatime.innerText = (date.toLocaleString("en-US", {timeZone: "Asia/Bangkok"})).replace(", ", " - ")
+  uswesttime.innerText = (date.toLocaleString("en-US", {timeZone: "America/Anchorage", weekday: "long", hour: "numeric", minute: "numeric", second: "numeric"})).replace(", ", " - ")
+  useasttime.innerText = (date.toLocaleString("en-US", {timeZone: "America/Chicago", weekday: "long",hour: "numeric", minute: "numeric", second: "numeric"})).replace(", ", " - ")
+  europetime.innerText = (date.toLocaleString("en-US", {timeZone: "Atlantic/Cape_Verde", weekday: "long", hour: "numeric", minute: "numeric", second: "numeric"})).replace(", ", " - ")
+  asiatime.innerText = (date.toLocaleString("en-US", {timeZone: "Asia/Bangkok", weekday: "long", hour: "numeric", minute: "numeric", second: "numeric"})).replace(", ", " - ")
 }, 1000)
